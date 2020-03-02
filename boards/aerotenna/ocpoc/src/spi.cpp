ce65c4bcb3c7468d5d3cfdfba6dc7548c4832598
Beat Küng <beat-kueng@gmx.net>
2020-03-02 16:18:04 +0100

    SPI config: use enum to specify bus

--- boards/aerotenna/ocpoc/src/spi.cpp
+++ boards/aerotenna/ocpoc/src/spi.cpp
@@ -35,7 +35,7 @@
 #include <drivers/drv_sensor.h>
 
 constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
-	initSPIBus(1, {
+	initSPIBus(SPI::Bus::SPI1, {
 		initSPIDevice(DRV_IMU_DEVTYPE_MPU9250, 0),
 		initSPIDevice(DRV_BARO_DEVTYPE_MS5611, 1),
 	}),
