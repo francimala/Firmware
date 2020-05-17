c8893ce0bd3221496f23034a64ac7c5b33fc07bb
Francesco <francesco.malacarne@gmail.com>
2020-05-17 11:32:52 +0200

    LEC command correctly passed to the module

--- src/drivers/distance_sensor/dwm1001/dwm1001.cpp
+++ src/drivers/distance_sensor/dwm1001/dwm1001.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2017-2019 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,253 +31,234 @@
  *
  ****************************************************************************/
 
+/*
+ * @file dwm1001.cpp
+ * DWM1001 driver
+ * @author Francesco Malacarne - francesco.malacarne@gmail.com
+ */
+
 #include "dwm1001.hpp"
 
-DWM1001::DWM1001(const char *port) :
-	ScheduledWorkItem(MODULE_NAME, px4::serial_port_to_wq(port)),
-	CDev(DWM1001_DEFAULT_PORT)
-{
-	PX4_INFO("DWM1001 function, the first one present, before strncpy");
+ __EXPORT int dwm1001_main(int argc, char *argv[]);
 
-	// store port name
-	strncpy(_port, port, sizeof(_port) - 1);
+ /* static variables */
+ static bool thread_should_exit = false;        /**< dwm1001 exit flag */
+ static bool thread_running = false;            /**< dwm1001 status flag */
+ static int dwm1001_task;                       /**< Handle of dwm1001 task / thread */
 
-	// enforce null termination
-	_port[sizeof(_port) - 1] = '\0';
-}
+ int set_uart_baudrate(const int _fd)
+ {
+  // baudrate 115200, 8 bits, no parity, 1 stop bit
+  unsigned speed = B115200;
+  termios uart_config{};
+  int termios_state{};
 
-DWM1001::~DWM1001()
-{
-	// make sure we are truly inactive
-	stop();
+  if(tcgetattr(_fd, &uart_config) < 0) {
+    PX4_ERR("Error from tcgetattr");
+    return false;
+  };
 
-	perf_free(_sample_perf);
-	perf_free(_comms_errors);
-}
+  // clear ONLCR flag (which appends a CR for every LF)
+  uart_config.c_oflag &= ~ONLCR;
 
-int
-DWM1001::init()
-{
-	// status
-	int ret = 0;
-	PX4_INFO("DWM1001 initialization started");
+  // set baud rate
+  if ((termios_state = cfsetispeed(&uart_config, speed)) < 0) {
+    PX4_ERR("CFG: %d ISPD", termios_state);
+    ret = -1;
+    return false;
+  }
 
-	do { // create a scope to handle exit conditions using break
+  if ((termios_state = cfsetospeed(&uart_config, speed)) < 0) {
+    PX4_ERR("CFG: %d OSPD", termios_state);
+    ret = -1;
+    return false;
+  }
 
-		// open fd
-		_fd = ::open(_port, O_RDWR | O_NOCTTY);
-
-		if (_fd < 0) {
-			PX4_ERR("Error opening fd");
-			return -1;
-		}
-
-		PX4_INFO("This is fd after opening the connection: %d",_fd);
-
-		// baudrate 115200, 8 bits, no parity, 1 stop bit
-		unsigned speed = B115200;
-		termios uart_config{};
-		int termios_state{};
-
-		tcgetattr(_fd, &uart_config);
-
-		// clear ONLCR flag (which appends a CR for every LF)
-		uart_config.c_oflag &= ~ONLCR;
-
-		// set baud rate
-		if ((termios_state = cfsetispeed(&uart_config, speed)) < 0) {
-			PX4_ERR("CFG: %d ISPD", termios_state);
-			ret = -1;
-			break;
-		}
-
-		if ((termios_state = cfsetospeed(&uart_config, speed)) < 0) {
-			PX4_ERR("CFG: %d OSPD\n", termios_state);
-			ret = -1;
-			break;
-		}
-
-		if ((termios_state = tcsetattr(_fd, TCSANOW, &uart_config)) < 0) {
-			PX4_ERR("baud %d ATTR", termios_state);
-			ret = -1;
-			break;
-		}
-
-		uart_config.c_cflag |= (CLOCAL | CREAD);	// ignore modem controls
-		uart_config.c_cflag &= ~CSIZE;
-		uart_config.c_cflag |= CS8;			// 8-bit characters
-		uart_config.c_cflag &= ~PARENB;			// no parity bit
-		uart_config.c_cflag &= ~CSTOPB;			// only need 1 stop bit
-		uart_config.c_cflag &= ~CRTSCTS;		// no hardware flowcontrol
-
-		/*
-		// setup for non-canonical mode
-		uart_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-		uart_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-		uart_config.c_oflag &= ~OPOST;
-
-		// fetch bytes as they become available
-		uart_config.c_cc[VMIN] = 1;
-		uart_config.c_cc[VTIME] = 1;
-		*/
-
-		if (_fd < 0) {
-			PX4_ERR("FAIL: laser fd");
-			ret = -1;
-			break;
-		}
-
-	} while (0);
-
-	// close the fd
-	::close(_fd);
-	_fd = -1;
-
-	PX4_INFO("I have closed the file, this is fd %d",_fd);
-
-	if (ret == PX4_OK) {
-		PX4_INFO("Start is launched");
-		start();
-	}
-
-	PX4_INFO("DWM1001 initialization compelted, the communication should be fine");
-	PX4_INFO("This is ret %d",ret);
-	px4_usleep(10000);
-
-	return ret;
-}
-
-int
-DWM1001::collect()
-{
-	perf_begin(_sample_perf);
-	PX4_INFO("DWM1001 collection started");
-
-	// clear buffer if last read was too long ago
-	int64_t read_elapsed = hrt_elapsed_time(&_last_read);
-
-	// the buffer for read chars is buflen minus null termination
-	int readlen = 36; // how many characters do I want to read? This value must be >= 36
-	char readbuf[readlen-1];
-
-	int ret = 0;
-//	float distance_m = -1.0f;
-
-//////////////////////////////////////////////////////////////////////////
-
-	// Check the number of bytes available in the buffer
-	int bytes_available = 0;
-	PX4_INFO("This is fd before using ioctl: %d",_fd);
-	::ioctl(_fd, FIONREAD, (unsigned long)&bytes_available);
-	px4_sleep(1);
-	PX4_INFO("ioctl performed in collect, bytes_available: %d", bytes_available);
+  uart_config.c_cflag |= (CLOCAL | CREAD);	// ignore modem controls
+  uart_config.c_cflag &= ~CSIZE;
+  uart_config.c_cflag |= CS8;			          // 8-bit characters
+  uart_config.c_cflag &= ~PARENB;			      // no parity bit
+  uart_config.c_cflag &= ~CSTOPB;			      // only need 1 stop bit
+  uart_config.c_cflag &= ~CRTSCTS;		      // no hardware flowcontrol
 
 
-	if (!bytes_available) {
-		PX4_INFO("No bytes available");
-		perf_end(_sample_perf);
-		return -EAGAIN;
-	}
+  // setup for non-canonical mode
+  uart_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+  uart_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+  uart_config.c_oflag &= ~OPOST;
+  // fetch bytes as they become available
+  uart_config.c_cc[VMIN] = 1;
+  uart_config.c_cc[VTIME] = 1;
 
-////////////////////////////////////////////////////////////////////////////
+  if ((termios_state = tcsetattr(_fd, TCSANOW, &uart_config)) < 0) {
+    PX4_ERR("baud %d ATTR", termios_state);
+    ret = -1;
+    return false;
+  }
 
+  tcflush(_fd, TCIOFLUSH); // flushes both data received but not read, and data written but not transmitted.
 
-	// parse entire buffer
-//	const hrt_abstime timestamp_sample = hrt_absolute_time(); // UNLOCK THISSSS
+  return true;
+ }
 
-	do {
-		// read from the sensor (uart buffer)
-		ret = ::read(_fd, &readbuf[0], readlen); // this is the KEY
-		PX4_INFO("I'm in the do component");
+ int uart_init(char const *uart_name)
+ {
+     int serial_fd = open(uart_name, O_RDWR | O_NOCTTY | O_SYNC);
+     //int serial_fd = open(uart_name, O_RDWR | O_NOCTTY); // not working
 
-		if (ret < 0) {
-			PX4_ERR("read err: %d", ret);
-			perf_count(_comms_errors);
-			perf_end(_sample_perf);
+     if (serial_fd < 0) {
+         PX4_ERR("failed to open port: %s", uart_name);
+         return false;
+     }
+     return serial_fd;
+ }
 
-			// only throw an error if we time out
-			if (read_elapsed > (kCONVERSIONINTERVAL * 2)) {
-				/* flush anything in RX buffer */
-				tcflush(_fd, TCIFLUSH);
-				return ret;
+ int
+ dwm1001_programming(const int _fd)
+ {
+  int num_bytes = 0;
+  // Flush the receive buffer: data received but not read.
+  tcflush(_fd, TCIFLUSH);
 
-			} else {
-				return -EAGAIN;
-			}
-		}
+  num_bytes = ::write(_fd, "reset\r", 6); // \r is working, it counts as 1 char
+  tcdrain(_fd);
 
-		_last_read = hrt_absolute_time();
+  hrt_abstime time_now = hrt_absolute_time();
+  const hrt_abstime timeout_usec = time_now + 2000000; // us
+  while (time_now < timeout_usec) {
+    time_now = hrt_absolute_time();
+  }
 
-		PX4_INFO("%c",readbuf[0]); // CHECK THIS
-///////////////////////////////////////////////////////////////////////////////////////
-/*
-		// parse buffer
-		for (int i = 0; i < ret; i++) {
-			dwm1001_parse(readbuf[i], _linebuf, &_linebuf_index, &_parse_state, &distance_m);
-		}
+  if(num_bytes != 6) {
+    PX4_ERR("Something went wrong with writing reset!");
+    return false;
+  }
 
-		// bytes left to parse
-		bytes_available -= ret;
+  num_bytes = ::write(_fd, "\r\r", 2); // \r is working
+  tcdrain(_fd);
 
-	} while (bytes_available > 0);
+  time_now = hrt_absolute_time();
+  const hrt_abstime timeout_usec_2 = time_now + 2000000; // us
+  while (time_now < timeout_usec_2) {
+    //PX4_INFO("time_now: %llu\ntimeout_usec: %llu", time_now, timeout_usec);
+    time_now = hrt_absolute_time();
+  }
 
-	// no valid measurement after parsing buffer
-	if (distance_m < 0.0f) {
-		perf_end(_sample_perf);
-		return -EAGAIN;
-	}
+  if(num_bytes != 2) {
+    PX4_ERR("Something went wrong with writing enter twice!");
+    return false;
+  }
 
-	// publish most recent valid measurement from buffer
-	_px4_rangefinder.update(timestamp_sample, distance_m);
-	*/
-///////////////////////////////////////////////////////////////////////////////////////////
-} while(0);
+  num_bytes = ::write(_fd, "lec\r", 4); // \r is working
+  tcdrain(_fd);
 
-	perf_end(_sample_perf);
+  if(num_bytes != 4) {
+    PX4_ERR("Something went wrong with writing lec!");
+    return false;
+  }
 
-	return PX4_OK;
-}
+  PX4_INFO("The dwm1001 module should be programmed with lec");
 
-void
-DWM1001::start()
-{
-	// schedule a cycle to start things
-	PX4_INFO("DWM1001 start");
-	ScheduleOnInterval(1000_ms);
-}
+  return true;
+ }
 
-void
-DWM1001::stop()
-{
-	PX4_INFO("DWM1001 stop");
-	ScheduleClear();
-}
+ void
+ usage(const char *reason)
+ {
+     if (reason) {
+         PX4_INFO("%s", reason);
+     }
 
-void
-DWM1001::Run()
-{
-	PX4_INFO("DWM1001 run");
-	// fds initialized?
-	if (_fd < 0) {
-		// open fd
-		_fd = ::open(_port, O_RDWR | O_NOCTTY);
-	}
+     PX4_INFO("usage: dwm1001 {start|stop}");
+     exit(1);
+ }
 
-	// perform collection
-	if (collect() == -EAGAIN) {
-		// reschedule to grab the missing bits, time to transmit 9 bytes @ 115200 bps
-		ScheduleClear();
-		ScheduleOnInterval(1000_ms, 87 * 9);
-		return;
-	}
-}
+ int
+ dwm1001_main(int argc, char *argv[])
+ {
 
-void
-DWM1001::print_info()
-{
-	printf("Using port '%s'\n", _port);
-	perf_print_counter(_sample_perf);
-	perf_print_counter(_comms_errors);
+     if (argc < 2) {
+         usage("missing command");
+     }
 
-	PX4_INFO("Still WIP");
+     if (!strcmp(argv[1], "start")) {
+
+         if (thread_running) {
+             PX4_INFO("running");
+             /* this is not an error */
+             exit(0);
+         }
+
+         thread_should_exit = false;
+         dwm1001_task = px4_task_spawn_cmd("dwm1001",
+                         SCHED_DEFAULT,
+                         SCHED_PRIORITY_DEFAULT,
+                         3000,
+                         dwm1001_thread_main,
+                         (char * const *)&argv[0]);
+         thread_running = true;
+         exit(0);
+     }
+
+     if (!strcmp(argv[1], "stop")) {
+         thread_should_exit = true;
+         exit(0);
+     }
+
+     if (!strcmp(argv[1], "status")) {
+         if (thread_running) {
+             PX4_INFO("running");
+
+         } else {
+             PX4_INFO("not started");
+         }
+
+         exit(0);
+     }
+
+ return 0;
+ }
+
+ int
+ dwm1001_thread_main(int argc, char *argv[])
+ {
+//     int cnt = 0;
+     int readlen = 10; // how many characters do I want to read? This value must be >= 36
+     char readbuf[readlen-1];
+//     char data[36];
+
+     PX4_INFO("The main task is now started, first the initializaiton, then the baudrate set");
+
+     //UART open
+     int uart = uart_init(DWM1001_PORT);
+     if(false == uart)return -1;
+     if(false == set_uart_baudrate(uart)){
+             PX4_INFO("ERROR: initialization failed");
+             return -1;
+     }
+     PX4_INFO("DWM1001 initialization succeded! This is uart: %d", uart);
+
+     if(false == dwm1001_programming(uart)) {
+       PX4_INFO("ERROR: DWM1001 programming failed");
+       return -1;
+     }
+
+     tcflush(uart, TCIOFLUSH);
+
+     PX4_INFO("Initialization compeleted, the endless loop starts");
+     while (!thread_should_exit) {
+
+       // ADDED
+       perf_begin(_sample_perf);
+
+       ret = ::read(uart, &readbuf[0], 1); // ret is the number of read values.
+
+       printf("%c", readbuf[0]);
+     }
+
+     thread_running = false;
+     close(uart);
+     perf_free(_sample_perf);
+     perf_free(_comms_errors);
+     return 0;
 }
